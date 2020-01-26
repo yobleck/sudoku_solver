8d2cbc16cb8f6c7767310e2037b59d192f9df4f8
yobleck <sennewell@gmail.com>
2020-01-26 12:29:06 -0500

    Add files via upload
    now removes values from sub squares based on inputs.
    calculates worst cases scenario number of iterations to check.
    formats potential solution to make them easier to read.

--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 #sudoku solver        worst case senario it takes 6.2350029e+60 (6 Novemdecillion) years
 #python solver.py 4 0:2 1:1 5:3 6:2 11:4 12:1
+#https://www.websudoku.com/?level=1&set_id=1818407932  python solver.py 9 6:2 8:1 10:6 14:3 17:7 18:7 20:8 21:4 24:5 27:2 29:3 30:6 32:9 33:1 34:7 37:7 38:6 40:1 42:3 43:4 46:8 47:1 48:3 50:2 51:9 53:6 56:4 59:6 60:7 62:5 63:5 66:1 70:9 72:6 74:2
 #http://www.sudoku-download.net/sudoku_4x4.php
 import sys;
 import math;
@@ -8,12 +9,13 @@ userIsStupid = False; #input error handling
 print("input: ");
 print(sys.argv);
 
-if (math.sqrt(int(sys.argv[1])))%1 != 0:
+if (math.sqrt(int(sys.argv[1])))%1 != 0 or int(sys.argv[1]) < 1:
     userIsStupid = True;
-    print("input size is not a square number");
+    print("input size is invalid. i.e. not square or less than one");
 
 #sudoku grid size i.e. 4x4 9x9 16x16 etc.
 size = int(sys.argv[1]);
+sqrt_size = int(math.sqrt(size));
 size_sum = int(((size**2)+size)/2);
 
 #############################################################################
@@ -40,26 +42,43 @@ for i in range(2,len(sys.argv)):
 #print(master_array);
 
 #############################################################################
-#TODO:remove input value from other cells based on square
 #similar to function above but calculates row column and square from input and remove value from those things
 for i in range(2,len(sys.argv)):
     pos = int(sys.argv[i].split(":")[0]);
     value = int(sys.argv[i].split(":")[1]);
-    row = math.floor(pos/size);
+    row = pos//size;
     col = pos%size;
     for j in range(0,size):
         if len(master_array[row*size+j]) > 1:
             try:
-                master_array[row*size+j].remove(value);
+                master_array[row*size+j].remove(value); #removes value from row
             except:
                 pass;
         if len(master_array[col+size*j]) > 1:
             try:
-                master_array[col+size*j].remove(value);
+                master_array[col+size*j].remove(value); #removes value from column
             except:
                 pass;
+    
+    sqr_row = row//sqrt_size;
+    sqr_col = col//sqrt_size;
+    sqr_thing = int(sqr_col*sqrt_size + sqr_row*math.pow(sqrt_size,3));
+    for k in range(0,sqrt_size):
+        for m in range(0,sqrt_size):
+            if len(master_array[(k*size+m)+sqr_thing]) > 1:
+                try:
+                    master_array[(k*size+m)+sqr_thing].remove(value); #removes value from square
+                except:
+                    pass;
 
 print(master_array);
+
+############################################################################
+wcs = 1; #calculates worst case senario number of iterations/combination the programs will have to search for a potential solution
+for x in range(0,len(master_array)):
+   wcs *= len(master_array[x]);
+print("worst case senario:");
+print(wcs);
 
 #############################################################################
 #array that temporarily hold possible solution to be checked
@@ -70,8 +89,8 @@ for i in range(0,size**2):
 
 #############################################################################
 #temp_solution = [2,1,4,3,4,3,2,1,3,2,1,4,1,4,3,2];
-#TODO:checker function check columns and square
-def checker(checkerarray): #input test array as local variable?
+#TODO:checker function check square
+def checker(checkerarray): #function to check if col row and square are valid
     isvalid = True;
     #if checkerarray == temp_solution:
         #solution_array.extend(checkerarray);
@@ -99,7 +118,10 @@ def func(tier):
         #print(testarray); #test function will go here
         if checker(testarray) == True:
             print("potential solution:");
-            print(testarray);
+            for x in range(0,size):
+                for y in range(0,size):
+                    print(testarray[x*size + y], end=" ");
+                print("");
         return;
     else:
         for i in range(0,len(master_array[tier])):
