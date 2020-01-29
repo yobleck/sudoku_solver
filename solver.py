0e4b3c66258c5fb06dca77422f2f05994772daf7
yobleck <sennewell@gmail.com>
2020-01-28 22:33:33 -0500

    Add files via upload
    dramatically reduced calculation time for 4x4 and easy (read very easy) 9x9 by recursively running the the two functions that eliminated all but the givens from their sqr and givens from the other tiles in the row/col/sqr
    
    mega fucking facepalm. shoulda done this earlier

--- solver.py
+++ solver.py
@@ -1,13 +1,18 @@
 #sudoku solver        worst case senario it takes 6.2350029e+60 (6 Novemdecillion) years
 #python solver.py 4 0:2 1:1 5:3 6:2 11:4 12:1
 #https://www.websudoku.com/?level=1&set_id=1818407932  python solver.py 9 6:2 8:1 10:6 14:3 17:7 18:7 20:8 21:4 24:5 27:2 29:3 30:6 32:9 33:1 34:7 37:7 38:6 40:1 42:3 43:4 46:8 47:1 48:3 50:2 51:9 53:6 56:4 59:6 60:7 62:5 63:5 66:1 70:9 72:6 74:2
+#https://www.websudoku.com/?level=4&set_id=2698854159  python solver.py 9 1:7 2:8 4:5 11:5 13:8 15:2 18:6 19:2 23:9 25:1 30:7 34:4 38:7 42:3 46:1 50:2 55:9 57:6 61:3 62:1 65:1 67:2 69:4 76:9 78:5 79:8
 #http://www.sudoku-download.net/sudoku_4x4.php
+#TODO: hidden singles aka values with 2 possibilities that can be quickly eliminated
+#TODO: used naked pair to eliminate those 2 possibilities from the rest of the row/col/sqr
+#TODO: remove sum check on row/col/sqr? might only need len(set())
 import sys;
 import math;
 userIsStupid = False; #input error handling
 #input prefilled cells in standard format position:value
-print("Input: ");
-print(sys.argv);
+given_array = sys.argv[2:];
+print("Input: ", end="");
+print(given_array); #TODO: fix bug where program crashes if no givens are provided
 
 if (math.sqrt(int(sys.argv[1])))%1 != 0 or int(sys.argv[1]) < 1:
     userIsStupid = True;
@@ -24,7 +29,7 @@ sqrt_size = int(math.sqrt(size));
 size_sum = int(((size**2)+size)/2);
 
 #############################################################################
-#create master array length=size^2 with each item being its own array 0-size
+#create master array length=size^2 with each item being its own array length 0 to size
 #array reads left to right top to bottom 
 #this array shows possible values for each cell and values should only be removed if absolutely sure
 master_array=[];
@@ -38,51 +43,71 @@ for i in range(size**2): #81=number of cells in sudoku
 
 #############################################################################
 #remove all but input value from appropriate cell in master array
-for i in range(2,len(sys.argv)):
-    pos = int(sys.argv[i].split(":")[0]);
-    value = int(sys.argv[i].split(":")[1]);
-    print(str(pos) +" : "+ str(value));
-    master_array[pos] = list(filter(lambda x: x==value, master_array[pos]));
-
-#print(master_array);
+def elim_all_but_given():
+    for i in given_array:
+        pos = int(i.split(":")[0]);
+        value = int(i.split(":")[1]);
+        #print(str(pos) +":"+ str(value)); #TODO: print in grid format
+        master_array[pos] = list(filter(lambda x: x==value, master_array[pos]));
 
 #############################################################################
 #similar to function above but calculates row column and square from input and remove value from those things
-for i in range(2,len(sys.argv)):
-    pos = int(sys.argv[i].split(":")[0]);
-    value = int(sys.argv[i].split(":")[1]);
-    row = pos//size;
-    col = pos%size;
-    for j in range(0,size):
-        if len(master_array[row*size+j]) > 1:
-            try:
-                master_array[row*size+j].remove(value); #removes value from row
-            except:
-                pass;
-        if len(master_array[col+size*j]) > 1:
-            try:
-                master_array[col+size*j].remove(value); #removes value from column
-            except:
-                pass;
-    
-    sqr_row = row//sqrt_size;
-    sqr_col = col//sqrt_size;
-    sqr_thing = int(sqr_col*sqrt_size + sqr_row*math.pow(sqrt_size,3));
-    for k in range(0,sqrt_size):
-        for m in range(0,sqrt_size):
-            if len(master_array[(k*size+m)+sqr_thing]) > 1:
+def elim_given_from_other():
+    for i in given_array:
+        pos = int(i.split(":")[0]);
+        value = int(i.split(":")[1]);
+        row = pos//size;
+        col = pos%size;
+        for j in range(0,size):
+            if len(master_array[row*size+j]) > 1:
                 try:
-                    master_array[(k*size+m)+sqr_thing].remove(value); #removes value from square
+                    master_array[row*size+j].remove(value); #removes value from row
                 except:
                     pass;
+            if len(master_array[col+size*j]) > 1:
+                try:
+                    master_array[col+size*j].remove(value); #removes value from column
+                except:
+                    pass;
+        
+        sqr_row = row//sqrt_size;
+        sqr_col = col//sqrt_size;
+        sqr_thing = int(sqr_col*sqrt_size + sqr_row*math.pow(sqrt_size,3));
+        for k in range(0,sqrt_size):
+            for m in range(0,sqrt_size):
+                if len(master_array[(k*size+m)+sqr_thing]) > 1:
+                    try:
+                        master_array[(k*size+m)+sqr_thing].remove(value); #removes value from square
+                    except:
+                        pass;
 
+#############################################################################
+def initial_clearing(): #this function recursively calls the elim functions until the value of given_array stops changing
+    #print("given");
+    global given_array;
+    #print(given_array);
+    elim_all_but_given();
+    elim_given_from_other();
+    temp_given_array = [];
+    for x in range(0,size**2):
+        if len(master_array[x]) == 1:
+           temp_given_array.append(str(x)+":"+str(master_array[x][0]));
+    #print("temp: ");
+    #print(temp_given_array);
+    if temp_given_array == given_array:
+        return;
+    else:
+        given_array = temp_given_array;
+        initial_clearing();
+initial_clearing();
+
+print("possibilities: ");
 print(master_array);
-
 ############################################################################
 wcs = 1; #calculates worst case senario number of iterations/combination the programs will have to search for a potential solution
 for x in range(0,len(master_array)):
    wcs *= len(master_array[x]);
-print("worst case senario number of possibilities to check:");
+print("worst case senario number of possibilities to check: ", end="");
 print(wcs);
 
 #############################################################################
@@ -128,15 +153,17 @@ def checker(checkerarray): #function to check if col row and square are valid
 
 ################################################################################
 #recursive function that goes through evey possible combination of solutions
+solution_counter = 0;
 def func(tier):
     if tier >= size**2:
-        #print(testarray); #test function will go here
         if checker(testarray) == True:
             print("potential solution:");
             for x in range(0,size):
                 for y in range(0,size):
                     print(testarray[x*size + y], end=" ");
                 print("");
+            global solution_counter;
+            solution_counter+=1;
         return;
     else:
         for i in range(0,len(master_array[tier])):
@@ -148,4 +175,7 @@ def func(tier):
             
 if userIsStupid == False:
     func(0);
+#print("");
+#print(solution_counter, end="");
+#print(" solution(s) found");
 print("end");
